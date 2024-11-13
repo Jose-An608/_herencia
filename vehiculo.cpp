60bb711fa0f64b88177a61eb53ef0e146c7c8528
Jose <jvilcanquic@unjbg.edu.pe>
2024-11-13 08:44:56 -0500

    Añadiendo programa de clases-ejercicio1

--- vehiculo.cpp
+++ vehiculo.cpp
@@ -14,7 +14,7 @@ class Vehiculo{
 		
 };
 
-class Automovil :: public Vehiculo{
+class Automovil : public Vehiculo{
 	private: 
 		int cantidadPuertas;
 		int cantidadPasajeros;
@@ -22,18 +22,18 @@ class Automovil :: public Vehiculo{
 	public:
 		Automovil(string, string, string, int, int, int); //Constructor2
 		void mostrarAutomovil();
-		int subirPasajeros();
-		int bajarPasajeros();
+		int subirPasajeros(int);
+		int bajarPasajeros(int);
 };
 
-/*class Camion::public Vehiculo{
+class Camion : public Vehiculo{
 	private:
 		string carga;
 	public:
-		Camion(string, string, string, int, float); //Constructor3
+		Camion(string, string, string, int, string); //Constructor3
 		void mostrarCamion();
-		void cambiarCarga();
-};*/
+		void cambiarCarga(string);
+};
 
 Vehiculo::Vehiculo(string _numeroPlaca, string _color, string _marca, int _velocimetro){
 	numeroPlaca = _numeroPlaca;
@@ -48,14 +48,14 @@ Automovil::Automovil(string _numeroPlaca, string _color, string _marca, int _vel
 	
 }
 
-/*Camion::Camion(string _numeroPlaca, string _color, string _marca, int _velocimetro, string):Vehiculo(){
+Camion::Camion(string _numeroPlaca, string _color, string _marca, int _velocimetro, string _carga) : Vehiculo(_numeroPlaca, _color, _marca, _velocimetro){
 	carga = _carga;
-}*/
+}
 
 void Vehiculo::mostrarDatos(){
 	cout << "Numero de Placa: " << numeroPlaca << endl;
 	cout << "Color: " << color << endl;
-	cout << "Marca de vehciulo: " << marca << endl;
+	cout << "Marca de vehiculo: " << marca << endl;
 	cout << "Velocimetro actual: " << velocimetro << endl;
 	
 }
@@ -74,29 +74,71 @@ void Automovil :: mostrarAutomovil(){
 	
 }
 
-/*int Automovil:: subirPasajeros(){
+int Automovil:: subirPasajeros(int n_Pasajeros){
+	int capMax=20;
+	
+	if(cantidadPasajeros + n_Pasajeros <= capMax){
+		cantidadPasajeros = cantidadPasajeros + n_Pasajeros;
+		cout << "Pasajeros aniadidos exitosamente. Hay un total de " << cantidadPasajeros << endl;
+		return cantidadPasajeros;
+	}else{
+		cout << "Se ha excedido con la capacidad maxima. No pueden subir" << n_Pasajeros << "pasajeros" << endl; 
+		return cantidadPasajeros;
+	}
+}
+
+int Automovil:: bajarPasajeros(int n_pasajerosBajados){
+	
+	if(n_pasajerosBajados <= cantidadPasajeros){
+		cantidadPasajeros = cantidadPasajeros - n_pasajerosBajados;
+		cout << "Han bajado " << n_pasajerosBajados << ". Quedarian un total de " << cantidadPasajeros << " pasajeros " << endl;
+		return cantidadPasajeros;
+	}else{
+		cout << "No se puede bajar " << n_pasajerosBajados << " ya que excede la cantidad actual de " << cantidadPasajeros << " pasajeros " << endl;
+		return cantidadPasajeros;
+	}
 	
 }
 
-int Automovil:: bajarPasajeros(){
-	
-}*/
-
-/*void Camion :: mostrarCamion(){
+void Camion :: mostrarCamion(){
 	mostrarDatos();
 	cout << "Carga: " << carga << endl;
 	
-}*/
+}
 
-/*void Camion :: cambiarCarga(){
+void Camion :: cambiarCarga(string n_carga){
+	carga = n_carga;
 	
-}*/
+	cout << "La nueva carga seria: " << carga << endl;
+}
 
 int main(){
-	Vehiculo vehiculo1(" 123D10 ", "Amarillo", "Nissan", 90);
-	vehiculo1.mostrarDatos();
 	
+	cout << "Datos del Vehiculo: " << endl;
+	Vehiculo vehiculo1("123D10 ", "Amarillo", "Nissan", 90);
+	vehiculo1.mostrarDatos();
 	vehiculo1.cambiarVelocidad(100);
 	
+	cout << endl;
+	
+	cout << "Datos del automovil: " << endl;
+	Automovil automovil1("21DE12", " Rojo", "Chevrolet", 90, 2, 15);
+	automovil1.mostrarAutomovil();
+	automovil1.subirPasajeros(3); //añadiendo 3 pasajeros
+	automovil1.bajarPasajeros(2); //bajando 2 pasajeros	
+	
+	cout << endl;
+	
+	cout << "Datos del camion: " << endl;
+	Camion camion1("34UD14", "Negro", "Volvo", 100, "Frutas");
+	camion1.mostrarCamion();
+	cout << endl;
+	camion1.cambiarCarga("Hortalizas");
+	cout << endl;
+	cout << "Mostrando datos del camion con la carga actualizada: " << endl;
+	camion1.mostrarCamion();
+	
+	system("pause");
+
 	return 0;
 }
